b26ed46d385737c9240921bfc96eb02cf1240ba1
JhulianDev <jhulian.dev@gmail.com>
2023-11-21 04:53:00 -0300

    Se crea estructura basica del componente Header y sus estilos

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { MaxWidth } from '../../assets/styles/generalStyles';
+import { HeaderContainer, Logo } from './HeaderStyles';
+import ISOTIPO from '../../assets/img/isotipo.png'
+import HamburgerButton from '../buttons/hamburguer/HamburgerButton';
 
 const Header = () => {
+  const [openNav, setOpenNav] = useState(false);
+  const showNav = () => setOpenNav(!openNav);
   return (
-    <div>
-      
-    </div>
+    <>
+      <HeaderContainer>
+        <MaxWidth $justify='space-between' $header>
+          <Logo src={ISOTIPO} alt='Isotipo IngenioWeb' />
+          <HamburgerButton $open={openNav} showNav={showNav} />
+        </MaxWidth>
+      </HeaderContainer>
+
+      <Outlet />
+    </>
   );
 };
 
---
+++ src/components/header/HeaderStyles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: -100px;
+`
+
+export const Logo = styled.img`
+  width: 60px;
+`
