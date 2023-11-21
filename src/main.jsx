25a87ba71aca044ca6bdf25213dffcf581f65b91
JhulianDev <jhulian.dev@gmail.com>
2023-11-21 03:54:05 -0300

    Se importan archivos .css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import { RouterProvider } from 'react-router-dom'
+import Router from './router/Router'
+import './assets/styles/Normalize.css'
+import './assets/fonts/Fonts.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={Router} />
 )
