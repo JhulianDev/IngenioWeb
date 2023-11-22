36d57c2c2ce3571ccf30228b84c545994a23427b
JhulianDev <jhulian.dev@gmail.com>
2023-11-22 03:35:20 -0300

    Se importa la seccion Beneficios a la aplicacion principal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Portada from './sections/portada/Portada';
+import Beneficios from './sections/beneficios/Beneficios';
 
 
 const App = () => {
   return (
-    <Portada />
+    <>
+      <Portada />
+      <Beneficios />
+    </>
   );
 };
 
