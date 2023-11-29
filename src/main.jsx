920699cd5e55cee154a2fb705b50f6bfd74a9786
JhulianDev <jhulian.dev@gmail.com>
2023-11-29 03:36:37 -0300

    Se aplica el uso de los contextos 'Currency' y 'Packs'

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,13 @@ import { RouterProvider } from 'react-router-dom'
 import Router from './router/Router'
 import './assets/styles/Normalize.css'
 import './assets/fonts/Fonts.css'
+import { CurrencyProvider } from './contexts/CurrencyContext'
+import { PacksProvider } from './contexts/PacksContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={Router} />
+  <CurrencyProvider>
+    <PacksProvider>
+      <RouterProvider router={Router} />
+    </PacksProvider>
+  </CurrencyProvider>
 )
